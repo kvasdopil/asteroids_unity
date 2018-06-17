38b2a4a056b0363d20f2dea593c3b0ca05040c4d
Alexey Guskov <lexag@mail.ru>
2018-06-17 19:56:12 +0200

    player can fly
    Signed-off-by: Alexey Guskov <lexag@mail.ru>

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+  private Rigidbody rb;
+
+  public float fwdSpeed = 10;
+  public float rotationSpeed = 10;
+  public float xMax = 10;
+  public float zMax = 10;
+  public float tilt = 1;
+
+  private void Start() {
+    rb = GetComponent<Rigidbody>();
+  }
+
+  private void wrap() {
+    float x = rb.position.x;
+    float z = rb.position.z;
+
+    if (x > xMax) {
+      x -= xMax * 2;
+    }
+
+    if (x < -xMax) {
+      x += xMax * 2;
+    }
+
+    if (z > zMax) {
+      z -= zMax * 2;
+    }
+
+    if (z < -zMax) {
+      z += zMax * 2;
+    }
+
+    rb.position = new Vector3(x, 0, z);
+  }
+
+  private void FixedUpdate() {
+    float moveHz = Input.GetAxis("Horizontal");
+    float moveVe = Input.GetAxis("Vertical");
+
+    rb.AddForce(rb.rotation * new Vector3(0, 0, 1) * (moveVe > 0 ? moveVe : 0) * fwdSpeed);
+
+    wrap();
+
+    float rot = rb.rotation.eulerAngles.y;
+    rb.rotation = Quaternion.Euler(0.0f, rot + moveHz * rotationSpeed, moveHz * -1 * tilt);
+  }
+}
